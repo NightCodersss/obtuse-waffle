9bb915b47c9cd7f883ee299e82c6a7d7d4525fce
justanothercoder <yanushviktor@gmail.com>
2015-10-28 20:38:56 +0300

    Tested loader

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "image_processor.hpp"
+#include "opencv_image_loader.hpp"
+#include "opencv_image.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 int main()
 {
