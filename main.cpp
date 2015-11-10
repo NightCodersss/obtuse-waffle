e723ccbc2bb0f8c477cf3df65a7403d7753230b6
justanothercoder <yanushviktor@gmail.com>
2015-11-10 17:36:31 +0300

    Command-line arguments and graceful error handling

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "image_processor.hpp"
 #include "opencv_image_loader.hpp"
 #include "opencv_image.hpp"
@@ -5,12 +6,25 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv_image_binarizer.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    auto loader = std::make_unique<OpenCVImageLoader>();
-    auto image = loader->load("a.png");
-    auto binarizer = std::make_unique<OpenCVImageBinarizer>();
-    auto bin = binarizer->binarize(*image);
-    ((OpenCVImage*)bin.get())->show();
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <image>\n";
+        return 0;
+    }
+
+    try 
+    {
+        auto loader = std::make_unique<OpenCVImageLoader>();
+        auto image = loader->load(argv[1]);
+        auto binarizer = std::make_unique<OpenCVImageBinarizer>();
+        auto bin = binarizer->binarize(*image);
+        ((OpenCVImage*)bin.get())->show();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << '\n';
+    }
+
     return 0;
 }
